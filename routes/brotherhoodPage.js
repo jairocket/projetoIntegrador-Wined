a5863e75b0cde6abb2509ae8eeba65c7fd17a41d
Silvio Souza <sjblu7@gmail.com>
2021-04-08 15:34:59 -0400

    Feito ajustes, finalizado partials Header e Footer e implementado partiasl na tela de confraria e Perfil

--- routes/brotherhoodPage.js
+++ routes/brotherhoodPage.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('brotherhoodPage');
+  res.render('brotherhoodPage', { title: "Confraria", style: "brotherhood" });
 });
 
 module.exports = router;
--- routes/profile.js
+++ routes/profile.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('profile');
+  res.render('profile', { title: "Meu Perfil", style: "profile" });
 });
 
 module.exports = router;
