7fe1bc6de0a2f5dde8b8db557e68d9392c750717
jairocket <jailsonanjos@hotmail.com>
2021-04-24 11:45:14 -0300

    css do termos de uso e privacidade

--- routes/privacyPolicy.js
+++ routes/privacyPolicy.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('privacyPolicy', { title: "Privacidade", style: "profile" });
+  res.render('privacyPolicy', { title: "Privacidade", style: "rules" });
 });
 
 module.exports = router;
