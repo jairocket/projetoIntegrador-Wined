9aa26711fbae97285cb39dea36bbca0f2f479791
jairocket <jailsonanjos@hotmail.com>
2021-04-29 21:38:06 -0300

    desafio parte2

--- routes/login.js
+++ routes/login.js
@@ -4,5 +4,6 @@ const userController = require('../controllers/UserController');
 
 /* GET users listing. */
 router.get('/', userController.loginForm);
+router.post('/', userController.userLogger);
 
 module.exports = router;
