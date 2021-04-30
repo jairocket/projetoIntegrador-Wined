62c594d5cb353746994f43b3ac1a935e34d47b8e
Gardinali-Gi <giovannagardinali11@gmail.com>
2021-04-30 16:24:05 -0300

    modificacao

--- routes/landingPage.js
+++ routes/landingPage.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('landingPage', { title: 'Wined' });
+  res.render('landingPage', { title: 'Wined', style: 'global' });
 });
 
 module.exports = router;
