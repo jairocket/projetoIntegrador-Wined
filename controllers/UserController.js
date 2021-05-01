612b60b2c463c6ffc39c9426003bb6bbaa893c91
jairocket <jailsonanjos@hotmail.com>
2021-05-01 13:30:03 -0300

    validações de login

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs');
 const fs = require('fs');
 const path = require('path');
+const { check, validationResult, body } = require('express-validator');
 
 let userJson = path.join("users.json")
 
@@ -9,14 +10,20 @@ let UserController = {
         res.render('register')
     },
     saveForm: (req, res)=>{
-        let users = JSON.parse(fs.readFileSync(userJson, 'utf-8'));
-        let {email, name, surname, password} = req.body;
-        let hashedPassword = bcrypt.hashSync(password, 12);
-        let user = {email, name, surname, password: hashedPassword};
-        users.push(user);
-        users = JSON.stringify(users, null, 2);
-        fs.writeFileSync(userJson, users);
-        res.send('usuário cadastrado com sucesso!');  
+        let errorsList = validationResult(req);
+        if (errorsList.isEmpty()){
+            let users = JSON.parse(fs.readFileSync(userJson, 'utf-8'));
+            let {email, name, surname, password, terms} = req.body;
+            let hashedPassword = bcrypt.hashSync(password, 12);
+            let user = {email, name, surname, terms, password: hashedPassword};
+            users.push(user);
+            users = JSON.stringify(users, null, 2);
+            fs.writeFileSync(userJson, users);
+            res.send('usuário cadastrado com sucesso!');
+        }else{
+            console.log(errorsList)
+            res.send("deu erro, negão")
+        }  
     },
     loginForm: (req, res)=>{
         res.render('login')
--- routes/register.js
+++ routes/register.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const userController = require('../controllers/UserController');
 const router = express.Router();
+const { check, validationResult, body } = require('express-validator');
 
 /* GET users listing. */
 router.get('/', userController.registerForm);
-router.post('/', userController.saveForm);
+router.post('/', [
+    check("name").not().isEmpty(),
+    check("surname").not().isEmpty(),
+    check("email").isEmail().normalizeEmail(),
+    check("password").isLength({min: 8}),
+    check("terms").exists()
+],    
+userController.saveForm);
 
 module.exports = router;
