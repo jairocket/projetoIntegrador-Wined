06a26c72cb49ff07c7e0cc28523eba85d96b5f49
jairocket <jailsonanjos@hotmail.com>
2021-05-23 16:29:21 -0300

    desafio30

--- routes/profile.js
+++ routes/profile.js
@@ -6,6 +6,8 @@ const auth = require('../middlewares/auth')
 const storage = require('../middlewares/multer');
 
 const upload = multer({storage: storage});
+const db = require('../database/models');
+const { Op } = require("sequelize");
 
 /* GET home page. */
 router.get('/', auth, function(req, res, next) {
@@ -15,6 +17,36 @@ router.get('/', auth, function(req, res, next) {
 router.post('/', upload.single('avatarFile'), function (req, res, next) {
   console.log(req.file);
   res.redirect('/perfil');
+});
+
+//Desafio 30
+
+router.get('/pesquisar/:offset?/:limit?', async (req, res)=>{
+  let limit = req.params.limit;
+  let offset = req.params.offset;
+  if(typeof(offset) == "undefined"){
+    offset = 0;
+  }
+  if(typeof(limit) == "undefined"){
+    limit = null;
+  }else{limit = parseInt(limit)}
+  
+  let usersFounded = await db.User.findAll({
+    limit: limit,
+    offset: parseInt(offset)
+  })
+  return res.json (usersFounded)
 })
+
+router.get('/filtrar/:name', async (req, res)=>{
+  let nameChecar = req.params;
+  let usersFounded = await db.User.findAll({
+    where: nameChecar
+  })
+  return res.json(usersFounded)
+});
+
+
+
 
 module.exports = router;
