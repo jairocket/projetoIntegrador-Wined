e7c9b0958e82fe20acf232e39f35c3bdca549f45
jairocket <jailsonanjos@hotmail.com>
2021-05-31 14:01:43 -0300

    brotherhood controller

---
+++ controllers/BrotherhoodController.js
@@ -0,0 +1,40 @@
+const db = require('../database/models');
+const Sequelize = require('sequelize');
+const { check, validationResult, body } = require('express-validator');
+
+const BrotherhoodController = {
+
+/*Creates a brotherhood */
+brotherhoodCreator: async (req, res) =>{ 
+    let {
+        name,  
+        description, 
+        background_Pic,
+        chancellor  
+        } = req.body;
+        
+    const brotherhood = await db.Brotherhood.create({
+        name,  
+        description, 
+        background_Pic, 
+        chancellor
+    })
+      console.log(brotherhood)
+      return res.json(brotherhood)
+    },
+    
+//GET brotherhood members
+
+getMembers: async (req, res) =>{
+    let id = req.params.id;
+    const brotherhoodMembers = await db.Brotherhood.findAll({
+      include: [
+        {model: db.User}
+      ],
+      where:{id} 
+    })
+    return res.json(brotherhoodMembers)
+    }
+
+}
+module.exports = BrotherhoodController
