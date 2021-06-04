8fdd03ccc6c7121c894b394f52a119551f9dcb0c
jairocket <jailsonanjos@hotmail.com>
2021-06-03 21:03:06 -0300

    ajustes callback para cadastro de confraria e chanceler

--- controllers/BrotherhoodController.js
+++ controllers/BrotherhoodController.js
@@ -9,17 +9,25 @@ brotherhoodCreator: async (req, res) =>{
     let {
         name,  
         description, 
-        background_Pic,
-        chancellor  
+        brotherhood_picture_id,
+        since 
         } = req.body;
         
     const brotherhood = await db.Brotherhood.create({
         name,  
         description, 
-        background_Pic, 
-        chancellor
+        brotherhood_picture_id, 
+        since
+    })
+
+    let brotherhood_id = brotherhood.id
+    const brotherhood_user = await db.Brotherhood_User.create({
+        brotherhood_id,
+        user_id: req.session.id,
+        chancellor: true
     })
       console.log(brotherhood)
+      console.log(brotherhood_user)
       return res.json(brotherhood)
     },
     
