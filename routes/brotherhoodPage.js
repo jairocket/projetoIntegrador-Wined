aa6134fbcc3033a6377f61ebf286c084991d429d
jairocket <jailsonanjos@hotmail.com>
2021-06-09 17:44:24 -0300

    eventCreator

--- routes/brotherhoodPage.js
+++ routes/brotherhoodPage.js
@@ -7,15 +7,22 @@ const brotherhoodController = require('../controllers/BrotherhoodController')
 const membershipCheck = require('../middlewares/membershipCheck');
 
 
+
+/*GET a form to create a brotherhood */
+
 router.get('/criar', (req, res) =>{
     res.render('brotherhoodCreator', {user: req.session.user, title: 'Cadastrar Confraria', style: 'register' })
-})
+});
 
 /*Creates a brotherhood */
 router.post('/criar', brotherhoodController.brotherhoodCreator);
 
 //GET brotherhood members 
 router.get('/confrades/:id', brotherhoodController.getMembers);
+
+router.get('/eventos/:id', (req, res) =>{
+    res.render('eventCreator', {user: req.session.user, title: 'Cadastrar Eventos', style: 'register'})
+}); 
 
 /* GET home page. */
 router.get('/:id', auth, membershipCheck, brotherhoodController.accessBrotherhood);
