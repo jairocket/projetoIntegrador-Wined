910a6db7bb9ec866b48ff4829afc43be853c4fc3
jairocket <jailsonanjos@hotmail.com>
2021-06-16 21:22:52 -0300

    update brotherhood

--- routes/brotherhoodPage.js
+++ routes/brotherhoodPage.js
@@ -9,13 +9,27 @@ const membershipCheck = require('../middlewares/membershipCheck');
 
 
 /*GET a form to create a brotherhood */
-
 router.get('/criar', auth, (req, res) =>{
     res.render('brotherhoodCreator', {user: req.session.user, title: 'Cadastrar Confraria', style: 'register' })
 });
 
 /*Creates a brotherhood */
 router.post('/criar', brotherhoodController.brotherhoodCreator);
+
+//GET brotherhoodEditor
+router.get('/editar/:id', auth, membershipCheck, (req, res)=>{
+    res.render('brotherhoodEditor', {
+        id: req.params.id,
+        user: req.session.user, 
+        title: 'Editar Confraria', 
+        style: 'register'})
+}) //criar view, incluir callback para renderizar a view
+
+/* UPDATE brotherhoodPage */
+router.put('/editar/:id', brotherhoodController.update);
+
+//DELETE brotherhood
+router.delete('/delete/:id', brotherhoodController.delete);
 
 //GET brotherhood members 
 router.get('/confrades/:id', auth, brotherhoodController.getMembers);
@@ -24,8 +38,10 @@ router.get('/eventos/:id', auth, (req, res) =>{
     res.render('eventCreator', {user: req.session.user, title: 'Cadastrar Eventos', style: 'register'})
 }); 
 
-/* GET home page. */
+/* GET brotherhoodPage. */
 router.get('/:id', auth, membershipCheck, brotherhoodController.accessBrotherhood);
+
+
 
 
 
