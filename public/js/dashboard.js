ebab622e1b87cbf65435f4e12111b2d0aef18cc0
jairocket <jailsonanjos@hotmail.com>
2021-07-26 10:02:10 -0300

    ajuste search wines back-end

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,17 +1,21 @@
-console.log('funcionando')
+console.log('funcionando');
 
-const btn = document.getElementById('btnBusca');
-console.log(btn);
-btn.onClick = async function(event){
-    console.log(btn)
-    if(event.target.dataset.type == "get-wines"){
-        let parameter = document.getElementById('txtBusca').innerText;
-        console.log(event.target.dataset.type);
-        const { data } = await axios.get(
-            `http://localhost:3000/dashboard/wines`, {
-                parameter
-            }
-        );
-        console.log(data);
-    }
+window.onload = ()=>{
+    const btn = document.getElementById('btnBusca');
+        console.log(btn);
+        btn.onClick = async function(event){
+            console.log(event)
+    // if(event.target.dataset.type == "get-wines"){
+    //     let parameter = document.getElementById('txtBusca').innerText;
+    //     console.log(event.target.dataset.type);
+    //     const { data } = await axios.get(
+    //         `http://localhost:3000/dashboard/wines`, {
+    //             parameter
+    //         }
+    //     );
+    //     console.log(data);
+    // }
 }
+
+}
+
--- services/DashboardService.js
+++ services/DashboardService.js
@@ -5,39 +5,46 @@ const nodemailer = require('../services/nodemailerService');
 const DashboardService = {
     getWines: async (req, res) =>{
         let {parameter} = req.body;
-        const wines = await db.Wine.findAll({
-            where: {
-                [Op.or]: [
-                    {
-                        wine_slug: {
-                            [Op.like]: parameter+'%'
+        if(!parameter){
+            const wines = await db.Wine.findAll()
+            return(wines)
+        }else{
+            const wines = await db.Wine.findAll({
+                where: {
+                    [Op.or]: [
+                        {
+                            wine_slug: {
+                                [Op.like]: parameter+'%'
+                            }
+                        },{
+                            appellation_slug: {
+                                [Op.like]: parameter+'%'
+                            }
+                        },{
+                            color: {
+                                [Op.like]: parameter+'%'
+                            }
+                        },{
+                            regions: {
+                                [Op.like]: parameter+'%'
+                            }
+                        },{
+                            country: {
+                                [Op.like]: parameter+'%'
+                            }
+                        },{
+                            vintage: {
+                                [Op.like]: parameter+'%'
+                            }
                         }
-                    },{
-                        appellation_slug: {
-                            [Op.like]: parameter+'%'
-                        }
-                    },{
-                        color: {
-                            [Op.like]: parameter+'%'
-                        }
-                    },{
-                        regions: {
-                            [Op.like]: parameter+'%'
-                        }
-                    },{
-                        country: {
-                            [Op.like]: parameter+'%'
-                        }
-                    },{
-                        vintage: {
-                            [Op.like]: parameter+'%'
-                        }
-                    }
-
-                ]
-            }
-        });
-        return(wines)
+    
+                    ]
+                }
+            });
+            return(wines)
+        }
+        
+        
     },
     favoriteWine: async (req, res)=>{
         let {id} = req.params;
