94d27ff7900997733b03550845e216cd8958dccb
jairocket <jailsonanjos@hotmail.com>
2021-08-13 13:37:54 -0300

    dashboard images

--- wined-front/src/components/top-view/index.js
+++ wined-front/src/components/top-view/index.js
@@ -1,18 +1,24 @@
 import './styles.css';
-// import Background from './assets/images/background-picture.svg';
-// import ProfilePicture from './assets/images/profile-picture.svg';
+import Background from './assets/images/background-picture.svg';
+import ProfilePicture from './assets/images/profile-picture.svg';
 export default function User(){
 
 
     return(
         <section>
             <div>
-                {/* <div className='top-background'>
-                    <Background />
-                </div> */}
-                {/* <div className='top-avatar'>
-                    <ProfilePicture />
-                </div> */}
+                <div className='top-background'>
+                    <img
+                        src={ Background }
+                        alt='Imagem - foto de background do usuário'
+                    />
+                </div>
+                <div className='top-avatar'>
+                    <img
+                        src={ ProfilePicture }
+                        alt= 'Imagem - avatar do usuário'
+                    />
+                </div>
                 <div className='top-user'>
                     <p>Some User</p>
                 </div>
